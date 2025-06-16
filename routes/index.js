84de10450d7c37a5cbf15df313b502304b1624cb
laurigust <101134587+laurigust@users.noreply.github.com>
2025-06-16 12:48:09 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Serve the index.html file for the root route
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app.js'));
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
